5867e01f45d21deb98f1e4624a9e601414f49aac
afsdzvcx123@gmail.com <afsdzvcx123@gmail.com>
2022-11-27 22:43:20 +0900

    6강 수강 완료.

---
+++ 3_nodardCorder/jbh/W01/js/greetings.js
@@ -0,0 +1,34 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault(); // 브라우저가 기본 동작 하지 못하게 막기
+ 
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    // form 보여준다.
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+
+} else {
+    // greeting 보여준다.
+    paintGreetings(savedUsername);
+}
