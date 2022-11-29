f96c41013cd68a39c469172ae8256b07b7f7f8b0
hooeee <dkrnl1318@naver.com>
2022-11-30 00:22:45 +0900

    faet : css 수정

--- 3_nodardCorder/kuh/scripts/quotes.js
+++ 3_nodardCorder/kuh/scripts/quotes.js
@@ -1,38 +1,17 @@
 let quotes = [
-    {
-        quote : 'Imagination is more important than knowledge',
-        author : 'Albert Einstein'
-    },
-    {
-        quote : 'A goal without a plan is just a wish.',
-        author : 'Antoine de Saint-Exupery'
-    },
-    {
-        quote : 'A wise man will make more opportunities than he finds.',
-        author : 'Sir Francis Bacon'
-    },
-    {
-        quote : 'Do not turn back when you are just at the goa',
-        author : 'Publilius Syrus'
-    },
-    {
-        quote : 'Energy and persistence conquer all things',
-        author : 'Benjamin Franklin'
-    },
-    {
-        quote : 'Never leave that `till tomorrow which you can do today.',
-        author : 'Benjamin Franklin'
-    },
-    {
-        quote : 'Everything comes to him who hustles while he waits.',
-        author : 'Thomas A. Edison'
-    },
-]
+  {
+    quote: "집에 가고 싶다",
+    author: "Kim Uihyeon.",
+  },
+  {
+    quote: "Everything comes to him who hustles while he waits.",
+    author: "Thomas A. Edison",
+  },
+];
 
-
-const quote = document.querySelector('div#quotes span:first-child');
-const author = document.querySelector('div#quotes span:last-child');
+const quote = document.querySelector("div#quotes span:first-child");
+const author = document.querySelector("div#quotes span:last-child");
 
 let index = Math.floor(Math.random() * quotes.length);
-quote.innerHTML = quotes[index].quote;
+quote.innerHTML = `오늘의 명언 | ${quotes[index].quote}`;
 author.innerHTML = quotes[index].author;
--- 3_nodardCorder/kuh/제이쿼리버전/scripts/background.js
+++ 3_nodardCorder/kuh/제이쿼리버전/scripts/background.js
@@ -1,6 +1,6 @@
 const bgDiv = $("#bg");
-const bgIndex = Math.floor(Math.random() * 3 + 1);
+const bgIndex = Math.floor(Math.random() * 2 + 1);
 const imgEl = $("<img/>");
 
-imgEl.attr("src", `./img/bg${bgIndex}.jpg`);
+imgEl.attr("src", `./img/bg3${bgIndex}.jpg`);
 bgDiv.append(imgEl);
