deea0967f9735dd21492e47d3acf9be6a489f19b
chungchung <cndgks0477@naver.com>
2022-11-29 21:18:04 +0900

    [LCH W04] Chap 8

---
+++ 3_nodardCorder/lch/W04/Chap 8/JS/weather.js
@@ -0,0 +1,19 @@
+const API_KEY = "";
+
+function onGeoOK(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#wather span:first-child");
+        const city = document.querySelector("#wather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+    alert("Can't find you. No weahter for you.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
