2d7143e578a4e179f1e039926678ec6b6996c0e1
sungyunahn <asy0239@github.com>
2022-12-04 23:22:35 +0900

    2022/12/04 | 코딩애플 | component 까지 완료

--- 4_reactAppleCoding/asy/workspace/blog/src/App.js
+++ 4_reactAppleCoding/asy/workspace/blog/src/App.js
@@ -1,26 +1,56 @@
+/* eslint-disable */
+
 import './App.css';
 import { useState } from 'react';
 
 function App() {
-  let [글제목, b] = useState([
-                              '남자 코드 추천', '강남 우동 맛집', '파이썬 독학'
+  let [글제목, 글제목변경] = useState([
+                              '남자 코트 추천', '강남 우동 맛집', '파이썬 독학'
                             ]);
+                
+  let [따봉, 따봉변경] = useState(0);
+
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
-      {
-        글제목.map((title) => (
-          <div className="list">
-            <h4>{ title }</h4>
-            <p>2월 17일 발행</p>
-          </div>
-        ))
-      }
+
+      <button onClick={() => {
+        let copy = [...글제목];
+        copy[0] = '여자 코트 추천';
+        글제목변경(copy);
+      }}>글 수정
+      </button>
+
+      <div className="list">
+        <h4>{글제목[0]} <span onClick={ () => {따봉변경(따봉+1)} }>👍</span>{ 따봉 }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]} <span>👍</span>{ 따봉 }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]} <span>👍</span>{ 따봉 }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      <Modal></Modal>
     </div>
   );
+}
+
+// 컴포넌트 
+function Modal(){
+    return (
+        <div className='modal'>
+          <h4>제목</h4>
+          <p>날짜</p>
+          <p>상세내용</p>
+        </div>
+    )
 }
 
 export default App;
