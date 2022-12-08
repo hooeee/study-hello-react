8f987186afa0ac4ab846ad62f4d874b7063b1d41
chungchung <cndgks0477@naver.com>
2022-12-08 21:31:01 +0900

    [LCH W06] 코드 길어지면 import export 하면 됩니다

--- 4_reactAppleCoding/lch/W06/Chap2/shop/src/App.js
+++ 4_reactAppleCoding/lch/W06/Chap2/shop/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data.js';
 
 function App() {
+
+  let [shoes, setShoes] = useState(data);
+  let img = ['https://codingapple1.github.io/shop/shoes1.jpg', 'https://codingapple1.github.io/shop/shoes2.jpg', 'https://codingapple1.github.io/shop/shoes3.jpg']
+
   return (
     <div className="App">
       <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">Jinyang Chemistry</Navbar.Brand>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
@@ -19,23 +25,25 @@ function App() {
       <div className='main-bg'></div>
       <div className='container'>
         <div className='row'>
-          <div className='col-md-4'>
-            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
-          <div className='col-md-4'>
-            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
-          <div className='col-md-4'>
-            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
+          {
+            img.map(function(a, i) {
+              return (
+                <ProductInfo img={a} title={shoes[i].title} content={shoes[i].content}></ProductInfo>
+              );
+            })
+          }
         </div>
       </div>
+    </div>
+  );
+}
+
+function ProductInfo(props) {
+  return (
+    <div className='col-md-4'>
+      <img src={props.img} width="80%"/>
+      <h4>{props.title}</h4>
+      <p>{props.content}</p>
     </div>
   );
 }
