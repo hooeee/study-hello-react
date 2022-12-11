92787bc58989757ebfd9b805618bb9ada43c5655
JDG <jang75967@gmail.com>
2022-12-11 19:58:23 +0900

    6주차 과제 | 코딩애플 React 강의 Part 2-7 까지 진행
    - styled-components 쓰면 CSS 파일 없어도 되는데

--- 4_reactAppleCoding/jdg/react/shop/src/pages/detail.js
+++ 4_reactAppleCoding/jdg/react/shop/src/pages/detail.js
@@ -1,12 +1,27 @@
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
-function Detail({ shoes }) {
+let YellowButton = styled.button`
+    background: ${props => props.bg};
+    color: ${props => props.bg == 'blue' ? 'white' : 'black'};
+    padding: 10px;
+`
+
+let Box = styled.div`
+    background: grey;
+    padding: 20px;
+`
+
+function Detail({ props, shoes }) {
 
     const { id } = useParams();
     const findShoe = shoes.find(x => x.id == id);
 
     return (
         <div className="container">
+            <YellowButton bg="blue">버튼</YellowButton>
+            <YellowButton bg="orange">버튼</YellowButton>
+            <Box>박스</Box>
             <div className="row">
                 <div className="col-md-6">
                     <img src={findShoe.img} width="100%" />
