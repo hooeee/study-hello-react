ccd694a9775d9cacdd1e5e89ae5d5a8c919790c9
DESKTOP-7SPR3J2\oksos <oksos0817@naver.com>
2022-12-28 00:16:21 +0900

    code: 최근 아이템 관련 스크롤 추가 및 아이템 관련 링크 연결

--- 4_reactAppleCoding/pkm/8주차/src/shop/src/RecentView.js
+++ 4_reactAppleCoding/pkm/8주차/src/shop/src/RecentView.js
@@ -1,32 +1,43 @@
 
 import './RecentView.css';
 import { Button, Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 
 function RecentView({ modeling }) {
+    const recentItem = (e) => {
+        const watchedItems = localStorage.getItem('watched');
+        const watchedItemsJson= JSON.parse(watchedItems);
+        watchedItemsJson.push(e);
+        const setArr = new Set(watchedItemsJson);
+        const remainItems = [...setArr];
+        localStorage.setItem('watched',JSON.stringify(remainItems));
+        }
     const watchedItems = localStorage.getItem("watched");
     const watchedItemsJson = JSON.parse(watchedItems);
     const copyLocalStorage = [...watchedItemsJson];
     console.log(modeling)
     console.log(copyLocalStorage)
     return (
+        <>
         <div className='recentTitle'>
         최근본 상품
+        </div>
         <div className='recentSub'>
         <Container>
-            <Row md={5}>
+            <Row md={3}>
                 {
                     copyLocalStorage.map(function (obj, i) {
                         return (
-                            <Col md={2}>
-                                <img className='img-main' src={'./basic' + modeling[obj].id + '.png'} width="70%" />
-                            </Col>
+                            <div class="col-md-4">
+                             <Link to={'/detail/' + modeling[obj].id } onClick={() => { recentItem(modeling[obj].id ) }}><img className='img-main' src={'./basic' + modeling[obj].id + '.png'} width="70%" /></Link>
+                            </div>
                         )
                     })
                 }
             </Row>
         </Container>
         </div>
-        </div>
+        </>
     )
 }
 
