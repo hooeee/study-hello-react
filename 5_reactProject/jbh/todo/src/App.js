e36f941a46b1607deb6f26ecd747f51754c2a16b
jobeomhee <afsdzvcx123@naver.com>
2023-01-12 23:10:23 +0900

    삽질중...하...

--- 5_reactProject/jbh/todo/src/App.js
+++ 5_reactProject/jbh/todo/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import Header from './Components/molecules/header/Header'
+import Main from './Components/molecules/main/Main'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="My header" subtitle="subtitle2" />
+      <Main message="My content" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
