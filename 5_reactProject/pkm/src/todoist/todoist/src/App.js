1031d9a228a07c740c06c3aef6dce405baaa41ca
Black <oksos0817@naver.com>
2023-01-14 00:13:43 +0900

    code: app 부분 수정

--- 5_reactProject/pkm/src/todoist/todoist/src/App.js
+++ 5_reactProject/pkm/src/todoist/todoist/src/App.js
@@ -2,14 +2,11 @@ import React from "react";
 import "./App.css";
 import { TitleNameInput } from "./components/organisms/TitleNameInput/TitleNameInput";
 function App() {
-<<<<<<< HEAD
   const item = [
     { value: "chocolate", label: "🍎 red" },
     { value: "strawberry", label: "🥏 blue" },
     { value: "vanilla", label: "🍯 yellow" },  
   ];
-=======
->>>>>>> parent of 102c77e (code: atomic Design 프로젝트 추가하기 부분 업로드)
   return (
     <div className="App">
       <TitleNameInput labelMainName={"프로젝트 추가"} labelName={"이름"} btnName="®"></TitleNameInput>
