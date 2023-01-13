14b4cfc8037f47daa9886077d854d4b41ace5b0e
jobeomhee <afsdzvcx123@naver.com>
2023-01-13 20:37:00 +0900

    후...

---
+++ 5_reactProject/jbh/todo/src/Components/molecules/iconbutton/IconButton.jsx
@@ -0,0 +1,5 @@
+import Button from "../../atoms/buttons/Button";
+
+export default function IconButton({ buttonName }) {
+    return <Button title={buttonName} variant={"btn bg-transparent"}></Button>;
+}
