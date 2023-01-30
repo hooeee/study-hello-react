872f76a7acad653c112791d7ae21c7e6cc8b78e1
JDG <jang75967@gmail.com>
2023-01-30 22:59:50 +0900

    상단바 색깔 표시

--- 5_reactProject/jdg/todoist/src/App.jsx
+++ 5_reactProject/jdg/todoist/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 
 import React, { Component } from "react";
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
@@ -13,6 +14,10 @@ import { createSlice, configureStore } from "@reduxjs/toolkit";
 function App() {
   return (
     <div className="App">
+      <div className="black-nav">
+        <button>MENU</button>
+        <h4 style={{ color: "white", fontSize: "30px" }}>To Do Ist</h4>
+      </div>
       <Button>Boot strap</Button>
     </div>
   );
