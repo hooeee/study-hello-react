1bfefe8df05c9bb2a8b68c72299868316244299a
chungchung <cndgks0477@naver.com>
2023-01-31 23:09:15 +0900

    Todo 프로젝트 생성

---
+++ 5_reactProject/lch/todo/src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MainPage from "./components/pages/mainPage/MainPage";
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage></MainPage>
+    </div>
+  );
+}
+
+export default App;
