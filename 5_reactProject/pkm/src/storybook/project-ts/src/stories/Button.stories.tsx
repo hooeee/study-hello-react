474b92a93337dd84fab71f8e93a8a015058295e8
Black <oksos0817@naver.com>
2023-02-05 17:00:38 +0900

    code: story book 적용테스트 완료

--- 5_reactProject/pkm/src/storybook/project-ts/src/stories/Button.stories.tsx
+++ 5_reactProject/pkm/src/storybook/project-ts/src/stories/Button.stories.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button } from "./Button";
+import { Button } from './Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "components/atomic/form/Button",
+  title: 'Example/Button',
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    backgroundColor: { control: "color" },
+    backgroundColor: { control: 'color' },
   },
 } as ComponentMeta<typeof Button>;
 
@@ -19,23 +19,29 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
+  primary: false,
+  label: 'Button',
+};
+export const Primary1 = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary1.args = {
   primary: true,
-  label: "Button",
+  label: 'Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: "Button",
+  label: 'Button',
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: "large",
-  label: "Button",
+  size: 'Large',
+  label: 'Button',
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: "small",
-  label: "Button",
+  size: 'Small',
+  label: 'Button',
 };
--- 5_reactProject/pkm/src/storybook/project-ts/src/stories/Button.tsx
+++ 5_reactProject/pkm/src/storybook/project-ts/src/stories/Button.tsx
@@ -1,26 +1,45 @@
-import React from "react";
-import css from "./Button.modules.css";
+import React from 'react';
+import css from './Button.module.css';
+
 interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
   primary?: boolean;
+  /**
+   * What background color to use
+   */
   backgroundColor?: string;
-  size?: "small" | "medium" | "large";
+  /**
+   * How large should the button be?
+   */
+  size?: 'Small' | 'Medium' | 'Large';
+  /**
+   * Button contents
+   */
   label: string;
+  /**
+   * Optional click handler
+   */
   onClick?: () => void;
 }
+
+/**
+ * Primary UI component for user interaction
+ */
 export const Button = ({
   primary = false,
-  size = "medium",
+  size = "Medium",
   backgroundColor,
   label,
   ...props
 }: ButtonProps) => {
   const mode = primary ? css.buttonPrimary : css.buttonSecondary;
   const sizeMode = {
-    small: css.buttonsmall,
-    medium: css.buttonmedium,
-    large: css.buttonlarge,
+    Small: css.buttonSmall,
+    Medium: css.buttonMedium,
+    Large: css.buttonLarge,
   };
-
   return (
     <button
       type="button"
